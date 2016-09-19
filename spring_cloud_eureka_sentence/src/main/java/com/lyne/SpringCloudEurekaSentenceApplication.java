f9c73a7f42931e88032c3b09ec264e9c8d48e0aa
LyneLiu <595596226@qq.com>
2016-09-19 19:22:53 +0800

    添加调用服务的相关实现

--- spring_cloud_eureka_sentence/src/main/java/com/lyne/SpringCloudEurekaSentenceApplication.java
+++ spring_cloud_eureka_sentence/src/main/java/com/lyne/SpringCloudEurekaSentenceApplication.java
@@ -2,8 +2,11 @@ package com.lyne;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SpringCloudEurekaSentenceApplication {
 
 	public static void main(String[] args) {
---
+++ spring_cloud_eureka_sentence/src/main/java/com/lyne/controller/HomeController.java
@@ -0,0 +1,42 @@
+package com.lyne.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.util.List;
+
+/**
+ * Created by nn_liu on 2016/9/19.
+ */
+@RestController
+public class HomeController {
+    @Autowired
+    private DiscoveryClient client;
+
+    @RequestMapping("/sentence")
+    public String getSentence() {
+        return getWord("eureka-subject") + " "
+                + getWord("eureka-verb") + " "
+                + getWord("eureka-article") + " "
+                + getWord("eureka-adjective") + " "
+                + getWord("eureka-noun") + ".";
+    }
+
+    private String getWord(String service) {
+        List<ServiceInstance> list = client.getInstances(service);
+        if (list != null && list.size() > 0) {
+            URI uri = list.get(0).getUri();
+            if (uri != null) {
+                return (new RestTemplate()).getForObject(uri, String.class);
+            }
+        }
+
+        return null;
+    }
+}
+
