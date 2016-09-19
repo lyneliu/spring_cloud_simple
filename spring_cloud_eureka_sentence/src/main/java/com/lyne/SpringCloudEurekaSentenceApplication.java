83b315c51c7350683651bc66a6ecf11ebd06b7e8
LyneLiu <595596226@qq.com>
2016-09-19 17:36:02 +0800

    初始化调用造句服务的构建项目

---
+++ spring_cloud_eureka_sentence/src/main/java/com/lyne/SpringCloudEurekaSentenceApplication.java
@@ -0,0 +1,12 @@
+package com.lyne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudEurekaSentenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEurekaSentenceApplication.class, args);
+	}
+}
