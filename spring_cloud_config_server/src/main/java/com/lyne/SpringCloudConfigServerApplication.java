30f533bdc42c8b327d16888e70838b6cd66b5680
LyneLiu <595596226@qq.com>
2016-09-19 10:17:35 +0800

    init spring cloud config server Note：注意配置名称的相关信息，spring boot在发布过程中的配置名称略有不同

---
+++ spring_cloud_config_server/src/main/java/com/lyne/SpringCloudConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.lyne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@EnableConfigServer
+@SpringBootApplication
+public class SpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+	}
+}
