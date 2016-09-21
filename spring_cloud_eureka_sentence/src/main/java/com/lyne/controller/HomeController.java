09f852fa214a9a558180bf9f49e14cb0824fdc7d
LyneLiu <595596226@qq.com>
2016-09-21 17:56:20 +0800

    eureka client添加ribbon依赖，通过客户端的负载均衡对eureka server中已注册的服务进行调用

--- spring_cloud_eureka_sentence/src/main/java/com/lyne/controller/HomeController.java
+++ spring_cloud_eureka_sentence/src/main/java/com/lyne/controller/HomeController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
@@ -18,6 +20,14 @@ public class HomeController {
     @Autowired
     private DiscoveryClient client;
 
+    /*通过Ribbon提供的负载均衡调用相关服务*/
+    @Autowired
+    RestTemplate restTemplate;
+
+    /**
+     * Eureka Client服务消费者
+     * @return
+     */
     @RequestMapping("/sentence")
     public String getSentence() {
         /* 调用不同的服务构造sentence */
@@ -28,6 +38,16 @@ public class HomeController {
                 + getWord("eureka-noun") + ".";
     }
 
+    /**
+     * Ribbon，客户端的负载均衡，实现Eureka Server中已注册的服务消费
+     * @return
+     */
+    @RequestMapping(value = "/ribbon",method = RequestMethod.GET)
+    @ResponseBody
+    public String getRibbonService(){
+        return restTemplate.getForEntity("http://eureka-client/word",String.class).getBody();
+    }
+
     private String getWord(String service) {
         List<ServiceInstance> list = client.getInstances(service);
         if (list != null && list.size() > 0) {
@@ -35,6 +55,9 @@ public class HomeController {
             /* 指定调用的URL */
             String url = uri.toString() + "/word";
             if (uri != null) {
+                /*获取host和port信息*/
+                //String word = (new RestTemplate()).getForObject(url, String.class);
+                //return "host:" + list.get(0).getHost()+",port:"+list.get(0).getPort()+",word:"+word;
                 return (new RestTemplate()).getForObject(url, String.class);
             }
         }
