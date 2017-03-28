7a7b8879b373a1aa8c86df4913aa499f42388993
LyneLiu <595596226@qq.com>
2017-03-28 18:46:40 +0800

    spring cloud config配置中心：配置文件的{application}默认可以映射至client的spring.application.name；{profile}默认可以映射至client的spring.profile.active。
    参考文档：https://cloud.spring.io/spring-cloud-config/spring-cloud-config.html

--- spring_cloud_config_server/src/main/java/com/lyne/SpringCloudConfigServerApplication.java
+++ spring_cloud_config_server/src/main/java/com/lyne/SpringCloudConfigServerApplication.java
@@ -1,9 +1,13 @@
 package com.lyne;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableConfigServer
 @SpringBootApplication
@@ -12,4 +16,5 @@ public class SpringCloudConfigServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
 	}
+
 }
--- spring_cloud_config_server/src/test/java/com/lyne/SpringCloudConfigServerApplicationTests.java
+++ spring_cloud_config_server/src/test/java/com/lyne/SpringCloudConfigServerApplicationTests.java
@@ -2,15 +2,29 @@ package com.lyne;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.cloud.config.environment.Environment;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = SpringCloudConfigServerApplication.class)
 public class SpringCloudConfigServerApplicationTests {
+
+	@Value("${local.server.port}")
+	private int port;
 
 	@Test
 	public void contextLoads() {
+		Environment environment = new TestRestTemplate().getForObject("http://localhost:"
+				+ port + "/", Environment.class);
+		assertFalse(environment.getPropertySources().isEmpty());
+		assertEquals("bar",
+				environment.getPropertySources().get(0).getSource().get("info.foo"));
 	}
 
 }
